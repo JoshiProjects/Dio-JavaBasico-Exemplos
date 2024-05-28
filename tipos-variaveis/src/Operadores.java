cc66ddc6f59582ff3687faf415e20e9fef37e94c
Joshi <josereinaldosoares94@gmail.com>
2024-05-28 20:55:35 -0300

    modulo de operadores finalizado

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int number1 = 1;
+        int number2 = 2;
+
+        boolean simNao = number1 == number2;
+
+        System.out.println("numero 1 é igual a numero2? " + simNao);
+
+        
+
+
+
+    }
+}
---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,6 @@
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        // a palavra reserva "final" é usada para representar um valor constante, Exeplo: final String CPF = "1651156161"
+    }
+}
